eaf948a1fb6f0db88a81ae359ab8f3cdca8a9f60
wengzehua <wengzehua@cvte.com>
2023-05-19 17:40:10 +0800

    feat: 引入Antd ConfigProvider

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "antd";
 
 function App() {
   return (
@@ -9,14 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <Button type="primary" href="https://reactjs.org">
           Learn React
-        </a>
+        </Button>
       </header>
     </div>
   );
